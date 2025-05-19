ac441c0e58aedf8539614473e1963fb8877fbff0
MARLEY018 <marleyantunes018@gmail.com>
2025-05-19 20:50:47 -0300

    Add files via upload

--- carta.c
+++ carta.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include "carta.h"
 
+void mostrarCarta(Carta carta) {
+    printf("Nome: %s\n", carta.nome);
+    printf("Força: %d\n", carta.forca);
+    printf("Velocidade: %d\n", carta.velocidade);
+    printf("Inteligência: %d\n", carta.inteligencia);
+}
